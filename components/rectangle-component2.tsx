8c79d55c5f9da901d9c5fd3a6c7139ee712065cd
Ayush Shrivastav <ayush@locofy.ai>
2022-11-03 12:01:00 +0000

    Pushed from Locofy Builder

---
+++ components/rectangle-component2.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./rectangle-component2.module.css";
+
+const RectangleComponent2: NextPage = () => {
+  return <div className={styles.rectangleDiv} />;
+};
+
+export default RectangleComponent2;
---
+++ components/rectangle-component3.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import styles from "./rectangle-component3.module.css";
+
+const RectangleComponent3: NextPage = () => {
+  return <textarea className={styles.rectangleTextarea} />;
+};
+
+export default RectangleComponent3;
